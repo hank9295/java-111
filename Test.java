11add6892c828fb69ea1a7e09157220559dd3755
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-09-30 10:40:47 +0800

    int add

--- Test.java
+++ Test.java
@@ -1,20 +1,20 @@
+import java.util.Scanner;
+
 public class Test{	
     public static void main(String[] args){
-        int num = 1;        
-        printStr(num);
-        num = 2;        
-        printStr(num);
-        num = num + 1;
-        printStr(num);
-        num += 1;        
-        printStr(num);
-        num++;        
-        printStr(num);
-        ++num;        
-        printStr(num);
-    }
-    
-    public static void printStr(int str){
-        System.out.println(str);
+        int num1;      
+        int num2; 
+        int sum;
+
+        Scanner input = new Scanner(System.in);        
+        System.out.print("Enter first integer:");
+        num1 = input.nextInt();
+        
+        System.out.print("Enter first integer:");
+        num2 = input.nextInt();
+        
+        sum = num1 + num2;
+        System.out.println("The sum is " + sum);
+        
     }
 }
