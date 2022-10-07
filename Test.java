d486ed899edbda234baec10b3b48724049481ffb
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-07 08:35:11 +0800

    score calculate

--- Test.java
+++ Test.java
@@ -8,12 +8,21 @@ public class Test{
         System.out.print("Enter first integer:");
         num1 = input.nextInt();
         
-        if (num1 >= 60 & num1 <=70) {
-            System.out.print("<=70");           
-        }   
-        else{
-            System.out.print("<60");
-        }    
+        if (num1 >= 90)
+            System.out.print("A");           
+        
+        else if (num1 >= 80)
+			System.out.print("B");
+			 
+		else if (num1 >= 70)
+            System.out.print("C");
+                      
+		else if (num1 >= 60)
+            System.out.print("D");
+					           
+		else
+            System.out.print("E");
+          
          
     }
 }
