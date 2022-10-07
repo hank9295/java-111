5503ed09b6f28dfbb098219de430b4da7c5abde1
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-07 09:01:45 +0800

    switch

--- Test.java
+++ Test.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 
 public class Test{	
     public static void main(String[] args){
-        int num1;
+        int grade;
         
         Scanner input = new Scanner(System.in);        
         System.out.print("Enter first integer:");
-        num1 = input.nextInt();
+        grade = input.nextInt();
         
-        if (num1 >= 90)
-            System.out.print("A");           
-        
-        else if (num1 >= 80)
-			System.out.print("B");
-			 
-		else if (num1 >= 70)
-            System.out.print("C");
-                      
-		else if (num1 >= 60)
-            System.out.print("D");
-					           
-		else
-            System.out.print("E");
+        switch(grade / 10 ){
+			case 9 :
+			    System.out.println("A");
+				break;
+			case 8 :
+				System.out.println("B");
+				break;
+			case 7 :
+				System.out.println("C");
+				break;
+			case 6 :
+				System.out.println("D");
+				break;
+			default  :
+				System.out.println("E");
+				break;
+		}		
           
          
     }
