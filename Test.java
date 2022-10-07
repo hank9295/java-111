090d11148463e4554f9fde97a8f8f4b8d85385fd
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-07 11:33:59 +0800

    while new

--- Test.java
+++ Test.java
@@ -2,32 +2,16 @@ import java.util.Scanner;
 
 public class Test{	
     public static void main(String[] args){
-        int grade;
-        
-        Scanner input = new Scanner(System.in);        
-        System.out.print("Enter first integer:");
-        grade = input.nextInt();
-		while(grade>100 | grade<0){
-               System.out.print("please enter again:");
-			   grade = input.nextInt();
-			}
-            switch(grade/10){
-			    case 10 :
-			    case 9 :
-			        System.out.println("A");
-				    break;
-				case 8 :
-					System.out.println("B");
-					break;
-				case 7 :
-					System.out.println("C");
-					break;
-				case 6:
-					System.out.println("D");
-					break;
-				default  :
-					System.out.println("E");
-					break;
-			}       
+        int grade = 0;
+		String space = "";
+             
+		while (grade < 10 ){
+            System.out.printf("%d %n", grade);
+			grade = grade + 1;
+			space = space + " ";
+			System.out.printf(space);
+			
+			
+		}
     }
 }
