b3fb407238c77fc8abb31e5b66e70f9f2552f71b
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-21 10:48:48 +0800

    用for print diamond

--- Test.java
+++ Test.java
@@ -1,10 +1,26 @@
 public class Test{	
     public static void main(String[] args){
-	    for ( int i=1; i<10; i++){
-			for ( int j=1; j<10; j++){
-			System.out.printf("%d*%d=%d\t",i,j,i*j);
+		int n = 5;
+		int m = 1;
+	    for( int j=1; j<=n; j++){
+		    for( int i=1; i<=(n-j); i++){
+			System.out.print(" ");
+			}
+			for( int i=1; i<=(2*j-1); i++){
+			System.out.print("*");
 			}
 			System.out.println();
-		}
+			}
+		for( int j=2; j<=n; j++){
+			for( int i=1; i<=(j-1); i++){	
+			System.out.print(" ");
+			}
+			for( int i=1; i<=((n-j)*2+1); i++){
+			System.out.print("*");
+			
+			}
+			System.out.println();
+			}
+			
 	}
 }	
