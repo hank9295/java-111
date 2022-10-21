09d7d39a7bfd9c2492b8d26e42e81d910e5fc13c
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-21 10:52:55 +0800

    用for print diamond

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 public class Test{	
     public static void main(String[] args){
 		int n = 5;
-		int m = 1;
 	    for( int j=1; j<=n; j++){
 		    for( int i=1; i<=(n-j); i++){
 			System.out.print(" ");
