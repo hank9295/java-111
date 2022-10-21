cc1a01775b3bbf12926d2ed6657c9b4e9633e1c9
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-21 11:20:37 +0800

    1+~100

--- Test.java
+++ Test.java
@@ -1,19 +1,9 @@
 public class Test{
     public static void main(String[] args){
-        int row=0,ans=5;
-        for(int i=1;i<ans*2;i++){
-            row=i;
-            if(row>ans){
-                row=ans*2-row;
-            }
-            for(int space=ans-row;space>0;space--){
-                System.out.printf(" ");
-            }
-            row=row*2-1;
-            for(int j=row;j>0;j--){
-                System.out.printf("*");
-            }
-            System.out.println();
-        }
+		int a = 0;
+       for (int i=1; i<=100; i++){
+		   a += i;
+	   }
+	    System.out.printf("%d",a);
     }
 }
