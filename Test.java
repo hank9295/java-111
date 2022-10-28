ef65c07a2fde55bded565c6feed61cb1391a3778
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-10-28 11:13:35 +0800

    new good

--- Test.java
+++ Test.java
@@ -1,11 +1,11 @@
 public class Test{
     public static void main(String[] args){
 	   for ( int i=0; i<6; i++) {
-		   for (int k=i-4; k<=i-4 +5; k++){
-			   if (k<1)
+		   for (int k=5; k>=0; k--){
+			   if (k>i)
 				   System.out.print("*");
 			   else
-				   System.out.printf("%d",k);
+				   System.out.printf("%d",i-k+1);
 		   }
 		   System.out.println("");
 	   }   
