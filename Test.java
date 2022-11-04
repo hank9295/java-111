92ede9e0785d730646df11da0d3725651e5378ca
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-11-04 10:01:32 +0800

    副程式

--- Test.java
+++ Test.java
@@ -1,13 +1,12 @@
 public class Test{
     public static void main(String[] args){
-	   for ( int i=0; i<6; i++) {
-		   for (int k=5; k>=0; k--){
-			   if (k>i)
-				   System.out.print("*");
-			   else
-				   System.out.printf("%d",i-k+1);
-		   }
-		   System.out.println("");
-	   }   
+        int a;
+		
+		a = add(7, 4);
+		System.out.printf("%d",a);
     }
+
+	public static int add(int x, int y){          
+		return x + y;
+	}	
 }
