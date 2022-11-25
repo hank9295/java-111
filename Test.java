e8e63ceb2b8d9fa518179bab586644dc0efe0a50
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-11-25 09:57:07 +0800

    new 空白菱形

--- Test.java
+++ Test.java
@@ -1,17 +1,21 @@
 public class Test{	
     public static void main(String[] args){
-		
+		int width=0;
 		for(int i=-4;i<=4;i++){
 			for(int y=-5;y<=5;y++){
-				if(i+y<=-5||i-y>=5)
+				if(Math.abs(y)>width)
 					System.out.printf("*"); 			
-				else if(i+y>4||i-y<-4)
-					System.out.printf("*");	
 				else
 					System.out.printf(" ");
 					
 			}	
-        System.out.println(); 			
+            System.out.println(); 
+
+			if (i<0)
+				width++;
+			else
+				width--;
+					
 		
 
 		}	
