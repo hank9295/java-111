033bedc541df46c290a91214f65f0678e0c8e166
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-11-25 09:40:08 +0800

    空白菱形

--- Test.java
+++ Test.java
@@ -1,30 +1,28 @@
-public class Test{
+public class Test{	
     public static void main(String[] args){
-        int a;
-		int k = 7;
 		
-		System.out.printf("%d%n",k);
+		for(int i=-4;i<=4;i++){
+			for(int y=-5;y<=5;y++){
+				if(i+y<=-5||i-y>=5)
+					System.out.printf("*"); 			
+				else if(i+y>4||i-y<-4)
+					System.out.printf("*");	
+				else
+					System.out.printf(" ");
+					
+			}	
+        System.out.println(); 			
 		
-		Test2 t1 = new Test2();
-		a = t1.add(k, 4);
-		System.out.printf("%d%n",a);
-    }
 
+		}	
+	}
 }
-
-class Test1{
-	public int add(int x, int y){          
-		//int t = add(x, y);
-		int t = x + y;
-		return t;
-	}	
-                  
-}
-class Test2{
-	public int add(int x, int y){          
-		//int t = add(x, y);
-		int t = x - y;
-		return t;
-	}	
-                  
-}
+/* ***** *****
+   ****   ****
+   ***     ***
+   **       **
+   *         *
+   **       **
+   ***     ***
+   ****   **** 
+   ***** ***** */
