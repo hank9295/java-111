e8a8bc0a0fbc1f0a6c35df5ab6102f71bcb923ff
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-12-09 10:39:24 +0800

    比大小

--- Test.java
+++ Test.java
@@ -1,21 +1,23 @@
 public class Test{	
     public static void main(String[] args){
-		int[] arr = {7, 3, 4, 9, 6, 10, 8, 5, 1, 2};
+		int[] arr = {9, 8, 7, 6, 5};
 		int i;
+		int count = 0;
 		for (int x=0; x<arr.length; x++){
-			for (int y=0; y<arr.length; y++){
+			for (int y=0; y<x-1; y++){
 				if (arr[x]<arr[y]){
 					i = arr[x];
 					arr[x]=arr[y];
 					arr[y]=i;
 				}
+			count++;
 			}	
 		}	
-				
 						
 		for (int x=0; x<arr.length; x++){			
 			System.out.println(">>" + arr[x]);
 		}	
+			System.out.printf("%d",count);
 	}
 	public static void add(int x) {
 		System.out.println("===>" + x);
