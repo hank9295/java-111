e65d5122788016de3cd99b5af869764f35d7ba48
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-12-16 10:11:13 +0800

    翻轉數字

--- Test.java
+++ Test.java
@@ -1,15 +1,19 @@
 public class Test{	
     public static void main(String[] args){
-		int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-		int count = 0;
-		int i;
-		for (int x=0; x<arr.length; x++){
-				if (arr[x] == 2){
-					System.out.println(">>"+x);
-					break;
-				}
-				count++;
+		int[] myArr = {7, 5, 4, 8, 1, 2};
+		int y;
+		for(int x=0; x<myArr.length/2; x++){
+				y = myArr[x];
+				myArr[x] = myArr[myArr.length-1-x];
+				myArr[myArr.length-1-x] = y;
+
+				
+				
+			
 		}
-		System.out.println("count>>" + count);
+		for (int i=0; i<myArr.length; i++){
+			System.out.println(myArr[i]);
+		}	
+		
 	}
 }		
