e624b3db9642357b6391257a761cafae1beafc4a
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-12-23 09:24:11 +0800

    不排序找中位數

--- Test.java
+++ Test.java
@@ -1,34 +1,43 @@
 public class Test{	
     public static void main(String[] args){
 		int[] myArr = {7, 5, 4, 9, 8, 1, 2};
-		int a;	
+		int[] flag =new int [myArr.length];
+		int flag_count = 1;	
 		int max=0;
-		int min=0;
-		int middle=0;
-		int average=0;
-		for(int x=0; x<myArr.length; x++){
-			average += myArr[x];
-			for (int y=0; y<x; y++){
-				if (myArr[x]<myArr[y]){
-					a = myArr[x];
-					myArr[x]=myArr[y];
-					myArr[y]=a;
+		int min=10;
+		int med=0;
+		int sum=0;
+		float avg;
+		for(int i=0;i<myArr.length; i++){
+			if(myArr[i]>max){
+				max=myArr[i];
+			}
+			if(myArr[i]<min){
+				min=myArr[i];
+			}
+			sum+=myArr[i];	
+		}	
+		avg = (float)sum / myArr.length;
+			
+		for(int i=0; i<4; i++){
+			int flag_min = 1000;
+			int flag_min_index = 0;
+			for (int j=0; j<myArr.length; j++){
+				if(myArr[j] < flag_min && flag[j] == 0){
+					flag_min = myArr[j];
+					flag_min_index = j;
 				}
 			}
-			
-			max = myArr[6]; 		
-			min = myArr[0];
-			middle = myArr[3];
+			flag[flag_min_index] = flag_count;
+			flag_count++;
+			med = myArr[flag_min_index];
 		}
-		System.out.printf("max:%d%n",max);//最大
-		System.out.printf("min:%d%n",min);//最小
-		System.out.printf("average:%d%n",average/7);//平均
-		System.out.printf("middle:%d%n",middle);//中位數
+
+		System.out.printf("max:%d%n",max);
+		System.out.printf("min:%d%n",min);
+		System.out.printf("avg:%f%n",avg);
+		System.out.printf("med:%d%n",med);
 		
-	
-		for (int x=0; x<myArr.length; x++){
-			System.out.print(">>"+myArr[x]);
-		}	
 		
 	}
 }		
