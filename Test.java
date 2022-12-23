c7f41283e20328879c93a31ce5df87e530a0b69d
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-12-23 11:34:26 +0800

    算出2個2個加起來的平均

--- Test.java
+++ Test.java
@@ -1,30 +1,14 @@
-import java.util.Scanner;
 public class Test{	
     public static void main(String[] args){
 		int[] myArr = {7, 5, 4, 9, 8, 1, 2};
-		int[] flag =new int [myArr.length];
-		int flag_count = 1;	
-		int med=0;
-		int number=0;
-		Scanner input = new Scanner(System.in);
-		System.out.print("number:");
-		number = input.nextInt();
-		for(int i=0; i<number; i++){
-			int flag_min = 1000;
-			int flag_min_index = 0;
-			for (int j=0; j<myArr.length; j++){
-				if(myArr[j] < flag_min && flag[j] == 0){
-					flag_min = myArr[j];
-					flag_min_index = j;
-				}
-			}
-			flag[flag_min_index] = flag_count;
-			flag_count++;
-			med = myArr[flag_min_index];
-		}
+		float a;
+		for(int i=0; i<5; i+=2){
+			a=myArr[i]+myArr[i+1];
+			
+			System.out.printf("number:%.1f%n",a/2);
 
-		System.out.printf("med:%d%n",med);
-		
+		}
+		System.out.printf("number:%d%n",myArr[myArr.length-1]);
 		
 		
 		
