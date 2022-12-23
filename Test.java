71d467a4bef0ee8f6d28e0ab8d0ab550c805a837
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-12-23 10:35:10 +0800

    輸入數字找陣列裡第幾個小的

--- Test.java
+++ Test.java
@@ -1,25 +1,15 @@
+import java.util.Scanner;
 public class Test{	
     public static void main(String[] args){
 		int[] myArr = {7, 5, 4, 9, 8, 1, 2};
 		int[] flag =new int [myArr.length];
 		int flag_count = 1;	
-		int max=0;
-		int min=10;
 		int med=0;
-		int sum=0;
-		float avg;
-		for(int i=0;i<myArr.length; i++){
-			if(myArr[i]>max){
-				max=myArr[i];
-			}
-			if(myArr[i]<min){
-				min=myArr[i];
-			}
-			sum+=myArr[i];	
-		}	
-		avg = (float)sum / myArr.length;
-			
-		for(int i=0; i<4; i++){
+		int number=0;
+		Scanner input = new Scanner(System.in);
+		System.out.print("number:");
+		number = input.nextInt();
+		for(int i=0; i<number; i++){
 			int flag_min = 1000;
 			int flag_min_index = 0;
 			for (int j=0; j<myArr.length; j++){
@@ -33,10 +23,9 @@ public class Test{
 			med = myArr[flag_min_index];
 		}
 
-		System.out.printf("max:%d%n",max);
-		System.out.printf("min:%d%n",min);
-		System.out.printf("avg:%f%n",avg);
 		System.out.printf("med:%d%n",med);
+		
+		
 		
 		
 	}
