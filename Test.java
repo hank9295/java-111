7522ded17ba2f781249ecab00b556be8172e949c
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-12-30 11:14:40 +0800

    找2在哪精簡版

--- Test.java
+++ Test.java
@@ -1,16 +1,28 @@
 public class Test{	
     public static void main(String[] args){
-		int[] myArr = {7, 5, 4, 9, 8, 1, 2};
-		float a;
-		for(int i=0; i<5; i+=2){
-			a=myArr[i]+myArr[i+1];
-			
-			System.out.printf("number:%.1f%n",a/2);
-
-		}
-		System.out.printf("number:%d%n",myArr[myArr.length-1]);
+		int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		int left = 0;
+		int right = arr.length-1;
+		int medianIndex;
+		int count = 0;
+		int numder = 2;
+		int numberIndex = 0;
 		
-		
-		
+			for (int x=0; x<3; x++){
+				medianIndex = (right - left)/2+left;
+				if (arr[medianIndex] == numder){
+					numberIndex = medianIndex;
+					break;		
+		        }
+				count++;
+				
+				if (numder > arr[medianIndex])
+					right = medianIndex/2;
+				else
+					left = medianIndex;
+					
+		    }
+		System.out.println("The position is>>" + numberIndex);	
+		System.out.println("count>>" + count);
 	}
-}		
+}	
