0ea9ff5a2001ad7326df47187e8cee0aba923ce3
hank9295 <114204224+hank9295@users.noreply.github.com>
2023-01-06 10:41:03 +0800

    同名方法

--- Test.java
+++ Test.java
@@ -1,21 +1,21 @@
 public class Test{	
     public static void main(String[] args){
-		int[] arr = {1, 2, 3, 4, 5, 6, 7};
-		int x = 10;
-		
-		setVariable(x);
-		System.out.println("setVariable = "+x);
-		
-		setVariable(arr);
-		for (int i:arr)
-			System.out.println("setVariable = "+i);
+	int x = 10;
+	double y = 10.0;
+	System.out.println("x " + setV(x));
+	System.out.println("x,y " + setV(x, 3));
+	System.out.println("y " + setV(y));
+	}	
+	
+	public static int setV(int x){
+		return x = 20;
+	}	
+	
+	public static int setV(int x, int y){
+		return x = 40;
 	}
 	
-	public static void setVariable(int x){
-		x = 20;
+	public static double setV(double x){
+		return x = 30.0;
 	}
-
-	public static void setVariable(int arr[]){
-		arr[0] = 20;
-	}	
 }	
