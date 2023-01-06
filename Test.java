e4baaf57e7b44859724a2c43697adad146b42bac
hank9295 <114204224+hank9295@users.noreply.github.com>
2023-01-06 10:54:04 +0800

    命令列引數

--- Test.java
+++ Test.java
@@ -1,21 +1,10 @@
 public class Test{	
     public static void main(String[] args){
-	int x = 10;
-	double y = 10.0;
-	System.out.println("x " + setV(x));
-	System.out.println("x,y " + setV(x, 3));
-	System.out.println("y " + setV(y));
+		int total = 0;
+		for(String str : args) {
+		total += Integer.parseInt(str);
+		System.out.println("args[] " + str);
+		}
+		System.out.println("total " + total);
 	}	
-	
-	public static int setV(int x){
-		return x = 20;
-	}	
-	
-	public static int setV(int x, int y){
-		return x = 40;
-	}
-	
-	public static double setV(double x){
-		return x = 30.0;
-	}
 }	
