28712b2772e37ce29bc534597178ef9932b7f4dd
hank9295 <114204224+hank9295@users.noreply.github.com>
2023-05-05 11:13:40 +0800

    sort排列

--- Test.java
+++ Test.java
@@ -1,17 +1,9 @@
-import java.util.Arrays;
-public class Test{
-	public static void main(String[] args){
-		int[] arr = {11, 2, 3, 14, 5, 16, 7};
-		
-		int min = Integer.MAX_VALUE;
-	
-		
-
-		Arrays.sort(arr);
-		
-		for(int e : arr)
-			System.out.println("e = " + e);
-			System.out.println();
-	}		
-
-}
+import java.util.Arrays; 
+public class Test{             
+	public static void main (String[] args){
+		int[] a = {8, 6, 7, 5, 1, 2, 9, 3, 4};
+		Arrays.sort(a);
+		for(int good: a)
+			System.out.print(good);
+	}
+}	
